892404f84cf2c1cff14d23220e986d602623ed11
Yashraj Singh <Yashraj2013216@akgec.ac.in>
2026-09-05 16:17:20 +0530

    Create User repository

---
+++ src/main/java/com/yashraj/taskmanager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yashraj.taskmanager.repository;
+
+import com.yashraj.taskmanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
